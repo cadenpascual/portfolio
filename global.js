0084d63ce7b8996978e09ba5ba18b52283eeb29f
cadenpascual <capascual@ucsd.edu>
2025-01-25 22:47:59 -0800

    check if works

--- global.js
+++ global.js
@@ -1,5 +1,18 @@
 console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
-  return Array.from(context.querySelectorAll(selector));
+    return Array.from(context.querySelectorAll(selector));
+  }
+  
+  // Select all navigation links
+navLinks = $$("nav a");
+  
+  // Find the link matching the current page's host and path
+currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+);
+  
+  // If a matching link is found, add the 'current' class
+if (currentLink) {
+    currentLink?.classList.add('current');
 }
