33871940896f214c446e8fc9a4c4f31ebe405333
cadenpascual <capascual@ucsd.edu>
2025-01-25 23:09:48 -0800

    js

--- global.js
+++ global.js
@@ -16,3 +16,30 @@ currentLink = navLinks.find(
 if (currentLink) {
     currentLink?.classList.add('current');
 }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'highlight/', title: 'Highlight' },
+
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+}
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+
+  
+  
